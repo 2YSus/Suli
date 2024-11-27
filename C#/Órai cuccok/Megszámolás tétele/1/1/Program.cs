ed177c5d4d5ab128960c618190fda35980713bea
Doodoosniff Cornelius Coochiesmith VII <134551098+2YSus@users.noreply.github.com>
2024-11-27 13:07:51 +0100

    rawr
    rwar

---
+++ C#/Órai cuccok/Megszámolás tétele/1/1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adj meg egy számot amelyiknek az osztóit szeretnéd tudni");
+            Console.Write("Szám: ");
+
+            int szam = int.Parse(Console.ReadLine());
+            Console.WriteLine($"A {szam} osztói: ");
+
+            for(int i = 1; i<=szam; i++)
+            {
+                if (szam%i==0)
+                {
+                    Console.Write($"{i}, ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
